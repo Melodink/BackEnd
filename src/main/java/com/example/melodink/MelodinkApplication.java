ee4870b1b764d4963963d24d52bbb82aefd9e04e
Parkjuseong319 <wntjd03199@gmail.com>
2026-04-16 21:48:05 +0900

    create project

---
+++ src/main/java/com/example/melodink/MelodinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.melodink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MelodinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/melodink/MelodinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.melodink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
